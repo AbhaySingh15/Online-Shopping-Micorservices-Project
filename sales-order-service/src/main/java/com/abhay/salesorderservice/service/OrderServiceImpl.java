d2b894c59920444bf0e50e99f223c49f81865cd7
Abhay Singh <singh.abhaysingh.abhay0009@gmail.com>
2022-09-07 13:30:46 +0530

    variable request params in restTemplate in sales order service

--- sales-order-service/src/main/java/com/abhay/salesorderservice/service/OrderServiceImpl.java
+++ sales-order-service/src/main/java/com/abhay/salesorderservice/service/OrderServiceImpl.java
@@ -39,30 +39,28 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public SalesOrderResponseModel createOrder(SalesOrderRequestModel orderRequestModel) {
-        TypeMap<SalesOrderRequestModel, SalesOrder> propertyMapper = modelMapper.createTypeMap(SalesOrderRequestModel.class, SalesOrder.class);
-        propertyMapper.addMapping(SalesOrderRequestModel::getOrder_desc,SalesOrder::setOrder_desc);
-        propertyMapper.addMapping(SalesOrderRequestModel::getOrder_date,SalesOrder::setOrder_date);
-        propertyMapper.addMapping(SalesOrderRequestModel::getCust_id,SalesOrder::setCustomer_sos);
-
-        SalesOrder salesOrder = this.modelMapper.map(orderRequestModel, SalesOrder.class);
+        mapSalesOrderRequestModelToSalesOrder();
+        SalesOrder salesOrder = modelMapper.map(orderRequestModel, SalesOrder.class);
         log.info(salesOrder.toString());
         Optional<CustomerSOS> customerSOS = customer_sos_repository.findById(orderRequestModel.getCust_id());
         if(customerSOS.isPresent()){
             salesOrder.setCustomer_sos(customerSOS.get());
-//            ItemList itemList = restTemplate.getForObject("http://localhost:9001/item?name=iphone-13,asus_zephyrus_g14", ItemList.class);
-//            List<Item> listOfItems = itemList.getItemList();
-            ResponseEntity<Item[]> responseEntity = restTemplate.getForEntity("http://localhost:9001/item?name=iphone-13,asus_zephyrus_g14",Item[].class);
+            StringBuilder getItemByNames = new StringBuilder("http://localhost:9001/item?name=");
+            String listOfItemNames = String.join(",",orderRequestModel.getItem_names());
+            log.info(listOfItemNames);
+            getItemByNames.append(listOfItemNames);
+            log.info(getItemByNames.toString());
+            ResponseEntity<Item[]> responseEntity = restTemplate.getForEntity(getItemByNames.toString(),Item[].class);
             Item[] itemArray = responseEntity.getBody();
             log.info(Arrays.toString(itemArray));
-            for(Item item: itemArray){
-                Order_Line_Item order_line_item = new Order_Line_Item();
-                order_line_item.setName(item.getName());
-                orderRepository.save(salesOrder);
-                order_line_item.setSalesOrder(salesOrder);
-                log.info(order_line_item.getName());
-                order_line_item_repository.save(order_line_item);
+            if(itemArray.length==0){
+                log.info("Order cannot be placed as there is no item available in the stock");
+                return null;
             }
-
+            orderRepository.save(salesOrder);
+            for(Item item: itemArray){
+                order_line_item_repository.save(createOrderLineItem(item,salesOrder));
+            }
             return null;
         }else{
             log.info("customer is not registered");
@@ -73,5 +71,20 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public ResponseEntity getOrderDetails(Long cust_id) {
         return null;
+    }
+
+    public Order_Line_Item createOrderLineItem(Item item,SalesOrder salesOrder){
+        Order_Line_Item order_line_item = new Order_Line_Item();
+        order_line_item.setName(item.getName());
+        order_line_item.setSalesOrder(salesOrder);
+        log.info(order_line_item.getName());
+        order_line_item_repository.save(order_line_item);
+        return order_line_item;
+    }
+
+    public void mapSalesOrderRequestModelToSalesOrder(){
+        TypeMap<SalesOrderRequestModel, SalesOrder> propertyMapper = modelMapper.createTypeMap(SalesOrderRequestModel.class, SalesOrder.class);
+        propertyMapper.addMapping(SalesOrderRequestModel::getOrder_desc,SalesOrder::setOrder_desc);
+        propertyMapper.addMapping(SalesOrderRequestModel::getOrder_date,SalesOrder::setOrder_date);
     }
 }
